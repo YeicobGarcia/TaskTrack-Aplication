8f1ecdf76af1bea5b534347929bc0d8e04df3cef
Yarissad <deb19975@uvg.edu.gt>
2024-11-06 17:33:35 +0000

    Actualización API

--- api.php
+++ api.php
@@ -19,7 +19,6 @@ $uri = $_SERVER['REQUEST_URI'];
 $path = explode('/', trim($uri, '/'));
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-
 $task_id_index = 2;
 $task_id = isset($path[$task_id_index]) ? $path[$task_id_index] : null;
 
@@ -31,7 +30,14 @@ switch ($requestMethod) {
         createTask();
         break;
     case 'GET':
-        getTasks();
+        if ($task_id) { 
+            // Obtener tarea específica por ID
+            error_log("Getting Task ID (GET): " . $task_id);
+            getTaskById($task_id);
+        } else {
+            // Obtener todas las tareas
+            getTasks();
+        }
         break;
     case 'PUT':
         if ($task_id) { 
@@ -54,7 +60,6 @@ switch ($requestMethod) {
 }
 
 // Función para crear una tarea
-
 function createTask() {
     global $client, $tableName;
 
@@ -75,7 +80,6 @@ function createTask() {
         }
 
         $newTaskId = str_pad($maxId + 1, 3, '0', STR_PAD_LEFT);
-
     
         $createdAt = date('Y-m-d H:i:s');
 
@@ -95,8 +99,6 @@ function createTask() {
         echo json_encode(['error' => $e->getMessage()]);
     }
 }
-
-
 
 // Función para obtener todas las tareas
 function getTasks() {
@@ -121,6 +123,34 @@ function getTasks() {
     }
 }
 
+// Función para obtener una tarea específica por ID
+function getTaskById($task_id) {
+    global $client, $tableName;
+
+    try {
+        $result = $client->getItem([
+            'TableName' => $tableName,
+            'Key' => [
+                'task_id' => ['S' => $task_id]
+            ]
+        ]);
+
+        if (isset($result['Item'])) {
+            $task = [
+                'task_id' => $result['Item']['task_id']['S'],
+                'title' => $result['Item']['title']['S'],
+                'description' => $result['Item']['description']['S'],
+                'status' => $result['Item']['status']['S'],
+                'created_at' => $result['Item']['created_at']['S']
+            ];
+            echo json_encode($task);
+        } else {
+            echo json_encode(['message' => 'Tarea no encontrada']);
+        }
+    } catch (AwsException $e) {
+        echo json_encode(['error' => $e->getMessage()]);
+    }
+}
 
 // Función para actualizar el estado de una tarea
 function updateTaskStatus($task_id) {
